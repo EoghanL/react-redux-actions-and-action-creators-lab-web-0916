6225134a902f05561287ba9de82115e3b254e68f
github username <github email address>
2016-11-21 15:59:52 -0500

    Done.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,32 @@
 // define your actions here!
+export function addMovie(obj){
+  return {
+    type: "ADD_MOVIE",
+    movie: {
+      title: obj.title,
+      year: obj.year
+    }
+  }
+}
+export function deleteMovie(obj){
+  return {
+    type: "DELETE_MOVIE",
+    movie: {
+      id: obj.id,
+      title: obj.title,
+      year: obj.year
+    }
+  }
+}
+export function updateUsername(str){
+  return {
+    type: "UPDATE_USERNAME",
+    username: str
+  }
+}
+export function resetUsername(){
+  return {
+    type: "RESET_USERNAME",
+    username: "default"
+  }
+}
